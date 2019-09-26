303fa0abc5fae584c9445d0c17e460ec4c9a78af
John Hasznosi <noctiscreo@gmail.com>
2019-09-26 14:42:52 +0100

    Exported database again.

---
+++ monsters_2019-09-26.sql
@@ -0,0 +1,51 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.01 (MySQL 5.7.27)
+# Database: monsters
+# Generation Time: 2019-09-26 13:41:50 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table monsters
+# ------------------------------------------------------------
+
+LOCK TABLES `monsters` WRITE;
+/*!40000 ALTER TABLE `monsters` DISABLE KEYS */;
+
+INSERT INTO `monsters` (`id`, `Name`, `Weapon`, `Challenge`)
+VALUES
+	(1,'Orc','Scimitar',0.5),
+	(2,'Bugbear','Morningstar',1),
+	(3,'Green Dragon','Breath',8),
+	(4,'Zombie','Slam',0.25),
+	(5,'Drider','Crossbow',6),
+	(6,'Kobold','Short Sword',0.125),
+	(7,'Ooze','Pseudopod',5),
+	(8,'Demon','Claws',6),
+	(9,'Mike','Sarcasm',0.125);
+
+/*!40000 ALTER TABLE `monsters` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
