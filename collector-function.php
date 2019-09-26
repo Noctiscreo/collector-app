c2b3bb276048267f0d3ee1feb2f1aaee2d328153
John Hasznosi <noctiscreo@gmail.com>
2019-09-26 11:08:20 +0100

    Added a dockblock to function page.

--- collector-function.php
+++ collector-function.php
@@ -1,5 +1,16 @@
 <?php
-
+/*
+ * Loops through each $monsters in 'monsters' database and
+ * adds html. Sends result to be displayed on the 'collector-display' page.
+ *
+ * @param $monsters = monsters in db
+ *
+ * @data 'Name' = string
+ * @data 'Weapon' = string
+ * @data 'Challenge' = float
+ *
+ * @return holds the html and db values.
+ */
 function monsterDisplay($monsters){
     $html = '';
     foreach($monsters as $monster){
